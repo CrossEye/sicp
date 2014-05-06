0c922ddc74800d8c74987dc247f902300767dff7
Scott Sauyet <scott.sauyet@gmail.com>
2014-05-05 23:56:06 -0400

    A few more exercises.

--- 1- Building Abstractions with Procedures/1.1/exercises.scm
+++ 1- Building Abstractions with Procedures/1.1/exercises.scm
@@ -1,6 +1,7 @@
 (require-extension test)
 
 ; 1.1.1 REPL fun
+; --------------
 (test "1.1.1 interpreter result" 10 10)
 (test "1.1.1 interpreter result" 12 (+ 5 3 4))
 (test "1.1.1 interpreter result" 8 (- 9 1))
@@ -9,6 +10,7 @@
 
 
 ; Ex 1.1
+; ------
 (test "Ex 1.1 ... 10 => 10" 10 10)
 (test "Ex 1.1 ... (+ 5 3 4) => 12" 12 (+ 5 3 4))
 (test "Ex 1.1 ... (- 9 1) => 8" 8 (- 9 1))
@@ -27,28 +29,23 @@
 
 
 ; Ex 1.2
+; ------
 (test "Ex 1.2 ... (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7))) => -37/150" (/ (- 37) 150) (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5))))) (* 3 (- 6 2) (- 2 7))))
 
 
 ; Ex 1.3
-;(define (square-top-two a b c)
-;  (cond 
-;    ((and (<= a b) (<= a c)) (+ (* b b) (* c c)))
-;    ((and (<= b a) (<= b c)) (+ (* a a) (* c c)))
-;    (else                    (+ (* a a) (* b b)))
-;  )
-;)
+; ------
+(define (square x) (* x x))
 
-(define (sum-squares list)
-  (if (null? list) 0 (+ (* (car list) (car list)) (sum-squares (cdr list))))
+(define (sum-squares a b)
+  (+ (square a) (square b))
 )
-(test "Ex 1.3 ... (sum-squares '(1 2 3 4)) => 30" 30 (sum-squares '(1 2 3 4)))
 
 (define (square-top-two a b c)
   (cond 
-    ((and (<= a b) (<= a c)) (sum-squares (list b c)))
-    ((and (<= b a) (<= b c)) (sum-squares (list a c)))
-    (else                    (sum-squares (list a b)))
+        ((and (<= a b) (<= a c)) (sum-squares b c))
+        ((and (<= b a) (<= b c)) (sum-squares a c))
+        (else                    (sum-squares a b))
   )
 )
 
@@ -59,7 +56,7 @@
 
 
 ; Ex 1.4
-
+; ------
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
@@ -71,4 +68,49 @@
 (test "Ex 1.4 ... (a-plus-abs-b 3 -5) => 8" 8 (a-plus-abs-b 3 -5))
 
 
+; Ex 1.5
+; ------
+;(define (p) (p))     ; (A)
+;
+;(define (test x y)   ; (B)
+;  (if (= x 0)
+;      0
+;      y))
+
+; Applicative order
+; -----------------
+; (test 0 (p))
+; => (test 0 (p))       ; (B)
+; => (if (= 0 0) 0 (p)) ; (A)
+; => 0                  ; `if`
+
+; Normal order
+; ------------
+; (test 0 (p))
+; => (if (= 0 0) 0 (p)) ; (B)
+; => (if (= 0 0) 0 (p)) ; (A) (substituting (p) in for (p) )
+; => (if (= 0 0) 0 (p)) ; (A) (substituting (p) in for (p) )
+; => (if (= 0 0) 0 (p)) ; (A) (substituting (p) in for (p) )
+; ...
+
+
+; Ex 1.6
+; ------
+
+;(define (new-if predicate then-clause else-clause)
+;  (cond (predicate then-clause)
+;        (else else-clause)))
+
+;(define (sqrt-iter guess x)
+;  (new-if (good-enough? guess x)
+;          guess
+;          (sqrt-iter (improve guess x)
+;                     x)))
+
+;(define (sqrt x)
+;  (sqrt-iter 1.0 x))
+
+;(sqrt 10) => infinite regress (presumably out-of-memory) as all the parameters
+;             to `new-if`, including the recursive call to sqrt-iter have to be
+;             parsed before sqrt-iter itself can return.
 
